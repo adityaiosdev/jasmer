cbadc34128d1fc753d12e768857fc7092d1bfbc6
Vivian Kosasih <viviankosasi38@gmail.com>
2021-08-03 22:17:25 +0700

    Storyline new version

--- jasmer/Controller/Custom Cells/ChapterCell.swift
+++ jasmer/Controller/Custom Cells/ChapterCell.swift
@@ -10,8 +10,5 @@ import UIKit
 class ChapterCell: UICollectionViewCell {
     
     @IBOutlet weak var chapterImage: UIImageView!
-    @IBOutlet weak var chapterCard: UIView!
-    @IBOutlet weak var chapterLabel: UILabel!
-    @IBOutlet weak var chapterLock: UIImageView!
     
 }
---
+++ jasmer/Controller/Custom Cells/InteractionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  InteractionCollectionViewCell.swift
+//  jasmer
+//
+//  Created by Vivian Kosasih on 03/08/21.
+//
+
+import UIKit
+
+class InteractionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var interactionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
