7926241c9279b1f7a8707558850cc6f7a47d1c53
Vivian Kosasih <viviankosasi38@gmail.com>
2021-08-05 14:45:47 +0700

    Adding post quiz plus fixing story page

--- jasmer/Controller/Custom Cells/InteractionView.swift
+++ jasmer/Controller/Custom Cells/InteractionView.swift
@@ -11,13 +11,14 @@ class InteractionView: UIView {
     
     var interactionDelegate: InteractionViewDelegate?
     var interactionList: [String:Int] = [:]
-    var isSelected: Bool = false
+    var currentStory: Storyline?
     var selectedSection = Int()
     var choices = [String]()
+//    var storyVC = StoryViewController()
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var interactionBox: UIView!
     @IBOutlet weak var interactionCollectionView: UICollectionView!
-//    weak var delegate: InteractionViewDelegate?
+    //    weak var delegate: InteractionViewDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,8 +28,16 @@ class InteractionView: UIView {
         super.init(coder: coder)
     }
     
+    public func initialSetup(){
+        nameLabel.layer.masksToBounds = true
+        nameLabel.layer.cornerRadius = 10
+        interactionBox.layer.cornerRadius = 10
+        interactionBox.layer.borderColor = UIColor(named: "Blue")?.cgColor
+        interactionBox.layer.borderWidth = 2
+    }
+    
     public func viewSetup(){
-        isSelected = false
+        initialSetup()
         let nib = UINib(nibName: "InteractionCollectionViewCell", bundle: nil)
         interactionCollectionView.register(nib, forCellWithReuseIdentifier: "interactionCell")
         
@@ -49,31 +58,41 @@ extension InteractionView: UICollectionViewDelegate, UICollectionViewDataSource,
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "interactionCell", for: indexPath) as! InteractionCollectionViewCell
-//        cell.translatesAutoresizingMaskIntoConstraints = true
-//        cell.interactionLabel.translatesAutoresizingMaskIntoConstraints = true
-//        cell.interactionLabel.frame.size = CGSize(width: CGFloat((choices[indexPath.row].count)*20), height: 35)
-//        cell.interactionLabel.text = choices[indexPath.row]
-//        cell.interactionLabel.sizeToFit()
-//        cell.interactionLabel.frame.inset(by: UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5))
+        //        cell.translatesAutoresizingMaskIntoConstraints = true
+        //        cell.interactionLabel.translatesAutoresizingMaskIntoConstraints = true
+        //        cell.interactionLabel.frame.size = CGSize(width: CGFloat((choices[indexPath.row].count)*20), height: 35)
+        //        cell.interactionLabel.text = choices[indexPath.row]
+        //        cell.interactionLabel.sizeToFit()
+        //        cell.interactionLabel.frame.inset(by: UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5))
         cell.initialSetup(choice: choices[indexPath.row])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let storyboard = UIStoryboard(name: "StoryStoryboard", bundle: nil)
-//        let vc = storyboard.instantiateViewController(identifier: "StoryStoryboard") as! StoryViewController
-//        vc.currentSection = interactionList[choices[indexPath.row]] as! Int
-//        vc.currentIndex = 0
-        
-        print("\(interactionList[choices[indexPath.row]])")
+        //        let storyboard = UIStoryboard(name: "StoryStoryboard", bundle: nil)
+        //        let vc = storyboard.instantiateViewController(identifier: "StoryStoryboard") as! StoryViewController
+        //        vc.currentSection = interactionList[choices[indexPath.row]] as! Int
+        //        vc.currentIndex = 0
+        print("\(interactionList[choices[indexPath.row]] ?? 0)")
+        if currentStory?.category == .interaction{
         interactionDelegate?.didTappedInteractions(selectedSection: interactionList[choices[indexPath.row]] ?? 0)
+        }
+        else if currentStory?.category == .miniGames{
+            interactionDelegate?.getSelectedText(selectedAnswer: choices[indexPath.row])
+            interactionDelegate?.getTappedSection(selectedSection: interactionList[choices[indexPath.row]] ?? 0)
+            interactionDelegate?.answerTapped()
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: choices[indexPath.row].count * 12, height: 45)
+//        return CGSize(width: choices[indexPath.row].count * 12, height: 44)
+        return CGSize(width: 200, height: 44)
     }
 }
 
-protocol InteractionViewDelegate: class{
+protocol InteractionViewDelegate: AnyObject{
     func didTappedInteractions(selectedSection: Int)
+    func getSelectedText(selectedAnswer:String)
+    func getTappedSection(selectedSection: Int)
+    func answerTapped()
 }
